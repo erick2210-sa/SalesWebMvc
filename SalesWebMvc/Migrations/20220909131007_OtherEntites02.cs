31a491cecd564f7aadf769ab79d31c6a1b2d1118
Erick Henrique <eksilvahenrique@gmail.com>
2022-09-09 10:13:31 -0300

    Other entities and second migration 02

---
+++ SalesWebMvc/Migrations/20220909131007_OtherEntites02.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class OtherEntites02 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
